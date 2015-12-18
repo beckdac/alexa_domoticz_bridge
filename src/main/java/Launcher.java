1ec5faf0df81eef2fe583fc8e54f3dab7fe18ce2
David Beck <beck.dac@live.com>
2015-12-18 01:19:24 -0800

    removed sdk unused targets; added an egg of the future to come

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -7,7 +7,6 @@
 
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-import helloworld.HelloWorldSpeechlet;
 import minecrafthelper.MinecraftSpeechlet;
 import domoticz.DomoticzSpeechlet;
 
@@ -22,8 +21,6 @@ import org.eclipse.jetty.server.SslConnectionFactory;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
-
-import session.SessionSpeechlet;
 
 import com.amazon.speech.Sdk;
 import com.amazon.speech.speechlet.Speechlet;
